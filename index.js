06a53c79036ea4f1fb902e4b679a5bafad292538
Anya <anya.s.litvinova@gmail.com>
2019-11-04 09:23:52 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = record => {
+  const thing = record.find(obj => obj.result === 'W');
+  if (thing === undefined) return undefined;
+  return thing.year;
+};
